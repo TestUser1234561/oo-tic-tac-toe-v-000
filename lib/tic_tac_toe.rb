93d908263b3ceb277ffbde575e5fcb78242450ad
Morgan Fudge <mfudge92@gmail.com>
2017-10-24 17:43:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,9 +72,9 @@ class TicTacToe
     index = input_to_index(gets.strip)
     if valid_move?(index)
       move(index, current_player())
-      display_board(board)
+      display_board()
     else
-      turn(board)
+      turn()
     end
   end
 
