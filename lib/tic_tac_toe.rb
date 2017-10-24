68985792559a368c9cb88b48d9d2b79405138344
Morgan Fudge <mfudge92@gmail.com>
2017-10-24 17:45:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,6 +18,14 @@ class TicTacToe
   end
 
   #Methods
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
   def input_to_index(n)
     return n.to_i - 1
   end
